10348601e2d0dafff3f4b462a04aa6368fe743b1
蛋蛋 <571532489@qq.com>
2020-10-28 22:10:23 +0800

    栈的测试

--- DataStructure/Stack/ArrayStackTest.java
+++ DataStructure/Stack/ArrayStackTest.java
@@ -1,7 +1,9 @@
 package zhan;
 
-public class ArrayStackTest {
+public class ArrayStackTest {//测试类
+
     public static void main(String[] args) {
+
         ArrayStack arrayStack = new ArrayStack(5);
 
         arrayStack.push(5);
@@ -10,10 +12,19 @@ public class ArrayStackTest {
         arrayStack.push(9);
         arrayStack.push(8);
 
+
+        System.out.println("==========栈中数据============");
+
+        System.out.println("栈顶:"+arrayStack.StackTop() );
+
         arrayStack.show();
-        System.out.println("======================");
+
+
+
         arrayStack.pop();
         arrayStack.pop();
+
+        System.out.println("==========栈中数据============");
 
         arrayStack.show();
 
