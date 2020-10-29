2a148ed66bc0883ccfe820fce59ed1e67709a205
蛋蛋 <571532489@qq.com>
2020-10-29 22:16:44 +0800

    单向链表

--- DataStructure/Linked list/SingleLianbiao/HeroNode.java
+++ DataStructure/Linked list/SingleLianbiao/HeroNode.java
@@ -1,16 +1,20 @@
 package lianbiao.SingleLianbiao;
 
 public class HeroNode {//链表的元素定义
-    public int no;
-    public String name;
-    public String nickname;
-    public HeroNode next;
+    //链表中的值
+    public int no;//编号
+    public String name;//名称
+    public String nickname;//别名
 
+    public HeroNode next;//下一个对象
+
+    //赋值
     public HeroNode(int no, String name, String nickname) {
         this.no = no;
         this.name = name;
         this.nickname = nickname;
     }
+
 
     @Override
     public String toString() {
